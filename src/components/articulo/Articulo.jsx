9813a272c96ad7380b5c3b3c9f0dae7328dc8052
Leandro <leandrojaime17@gmail.com>
2024-12-17 16:03:29 -0300

    card articulo styles

--- src/components/articulo/Articulo.jsx
+++ src/components/articulo/Articulo.jsx
@@ -7,6 +7,8 @@ const Articulo = ({ addToCart }) => {
   const { id } = useParams();
   const navigate = useNavigate();
   const [articulo, setArticulo] = useState(null);
+  const [talle, setTalle] = useState('');
+  const [color, setColor] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -17,6 +19,14 @@ const Articulo = ({ addToCart }) => {
 
     fetchData();
   }, [id]);
+
+  const handleTalleClick = (talle) => {
+    setTalle(talle);
+  };
+
+  const handleColorClick = (color) => {
+    setColor(color);
+  };
 
   if (!articulo) {
     return <div>Artículo no encontrado</div>;
@@ -31,10 +41,39 @@ const Articulo = ({ addToCart }) => {
       <img src={articulo.img} alt={articulo.nombre} />
       <div className='articleTextBox'>
         <h1>{articulo.nombre}</h1>
-        <p>${articulo.precio}</p>
+        <p className='price'>${articulo.precio}</p>
         <p>{articulo.descripcion}</p>
+        <div className='selectOptions'>
+          <label>Talle:</label>
+          <div className='optionsContainer'>
+            {['38', '39', '40', '41', '42'].map(talleOption => (
+              <div 
+                key={talleOption} 
+                className={`optionSquare ${talle === talleOption ? 'selected' : ''}`} 
+                onClick={() => handleTalleClick(talleOption)}
+              >
+                {talleOption}
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className='selectOptions'>
+          <label>Color:</label>
+          <div className='optionsContainer'>
+            {['rojo', 'azul', 'verde', 'negro', 'blanco'].map(colorOption => (
+              <div 
+                key={colorOption} 
+                className={`optionSquare ${color === colorOption ? 'selected' : ''}`} 
+                onClick={() => handleColorClick(colorOption)}
+                style={{ backgroundColor: color === colorOption ? '#343a40' : '#f5f5f7', color: color === colorOption ? '#fff' : '#333' }}
+              >
+                {colorOption}
+              </div>
+            ))}
+          </div>
+        </div>
         <div className='articleButtons'>
-          <button className='button-price' onClick={() => addToCart(articulo)}> 
+          <button className='button-price' onClick={() => addToCart({...articulo, talle, color})}> 
             <span>+ Agregar al carrito</span>
           </button>
           <button className='button-price' onClick={handleFinalizar}>
