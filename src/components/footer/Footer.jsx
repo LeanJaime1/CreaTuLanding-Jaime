3898d62b9477d296fac47f4c71a480a76c55c417
Leandro <leandrojaime17@gmail.com>
2024-12-18 22:22:47 -0300

    footer config

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import './footer.css';
 const Footer = () => {
   return (
     <div className='bg-dark'>
-      <footer className='footer py-3 my-4'>
+      <footer className='footer py-3'>
         <ul className="nav justify-content-center border-bottom pb-3 mb-3">
           <li className="nav-item"><a href="#" className="nav-link px-2 text-light">Home</a></li>
           <li className="nav-item"><a href="#" className="nav-link px-2 text-light">Productos</a></li>
